31cd11eb06eb9b6410281c3f7df2fccc12e62183
Regina <reginaabena.tech@hotmail.com>
2023-12-19 11:01:40 +0000

    [ROB]| completing task and functionality working

--- script.js
+++ script.js
@@ -88,19 +88,69 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+function passwordLength() {
+  let lengthOfPassword = parseInt(prompt("Please enter the desired characters for your password between 8 - 128"));
+
+  if (isNaN(lengthOfPassword) || lengthOfPassword < 8 || lengthOfPassword > 128) {
+    return null;
+  }
+  return lengthOfPassword;
+}
+
 // Function to prompt user for password options
 function getPasswordOptions() {
+  let lengthOfPassword = passwordLength();
+
+  let lowercaseCharacter, uppercaseCharacter, numericCharacter, specialCharacter;
+
+  do {
+    lowercaseCharacter = confirm("Include lowercase characters?");
+    uppercaseCharacter = confirm("Include uppercase characters?");
+    numericCharacter = confirm("Include numeric characters?");
+    specialCharacter = confirm("Include special characters?");
+  } while (!(lowercaseCharacter || uppercaseCharacter || numericCharacter || specialCharacter));
+
+  return {
+    length: lengthOfPassword,
+    lowerCase: lowercaseCharacter,
+    upperCase: uppercaseCharacter,
+    numeric: numericCharacter,
+    special: specialCharacter,
+  };
 
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  if (arr.length === 0) {
+    return null;
+  }
+  const randomElementIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomElementIndex];
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  const options = getPasswordOptions();
 
+  if (!options) {
+    return null;
+  }
+
+  const characterArrays = [
+    { condition: options.lowerCase, characters: lowerCasedCharacters },
+    { condition: options.upperCase, characters: upperCasedCharacters },
+    { condition: options.numeric, characters: numericCharacters },
+    { condition: options.special, characters: specialCharacters }
+  ];
+
+  const selectedCharacters = characterArrays
+    .filter(option => option.condition)
+    .flatMap(option => option.characters);
+
+  const password = Array.from({ length: options.length }, () => getRandom(selectedCharacters)).join('');
+
+  return password;
 }
 
 // Get references to the #generate element
